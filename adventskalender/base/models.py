c9e2a109b15724899392d5fa2600b6fd9cd80e5f
laDanz <cdanzmann@gmail.com>
2015-11-02 20:49:47 +0100

    fix encoding problems

--- adventskalender/base/models.py
+++ adventskalender/base/models.py
@@ -1,15 +1,18 @@
+# coding=utf8
+from __future__ import unicode_literals
+
 from django.core.urlresolvers import reverse
+from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
 from google.appengine.api import images as gimages
 from google.appengine.ext import blobstore
-
 
 class Reward(models.Model):
 	text = models.TextField(max_length=2000)
 	key = models.CharField(max_length=64, unique=True)
 	#file
 	#foreign keys
-	def __str__(self):
+	def __unicode__(self):
 		return self.key
 
 class Image(models.Model):
@@ -50,7 +53,7 @@ class Condition(models.Model):
 	valid_til = models.DateTimeField(null=True, blank=True)
 	#foreign keys
 	reward = models.ForeignKey(Reward)
-	def __str__(self):
+	def __unicode__(self):
 		return "from " + str(self.valid_from) + " to " + str(self.valid_til)
 
 class Riddle(models.Model):
@@ -59,19 +62,19 @@ class Riddle(models.Model):
 	#file
 	#foreign keys
 	reward = models.ForeignKey(Reward)
-	def __str__(self):
+	def __unicode__(self):
 		return self.question[:30]
 
 class Hint(models.Model):
 	hint = models.TextField(max_length=2000)
 	#foreign keys
 	riddle = models.ForeignKey(Riddle)
-	def __str__(self):
+	def __unicode__(self):
 		return self.hint[:30]
 
 class Choice(models.Model):
 	text = models.TextField(max_length=2000)
 	#foreign keys
 	riddle = models.ForeignKey(Riddle)
-	def __str__(self):
+	def __unicode__(self):
 		return self.text[:30]
